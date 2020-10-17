1841c177e1ef7eab99f797d544a26a654c59e3b3
Ernest Gu <ernest.gu.3.14@gmail.com>
2020-10-17 07:55:46 -0700

    Added help command (closes #13) and animation error checking
    fixes #3.

--- ClippyAssist/ClippyAssist.lua
+++ ClippyAssist/ClippyAssist.lua
@@ -168,5 +168,27 @@ end
 --------------------
 SLASH_CLIPPY1 = "/clippy"
 function SlashCmdList.CLIPPY(msg, editBox)
-	QueueAnimation(msg)	-- no error checking!
+	if msg == "" then
+		msg = "-help"
+	end
+	if msg == "-help" or msg == "-h" or msg == "-?" then
+		print("You are currently using Clippy Assist v" ..
+			GetAddOnMetadata("ClippyAssist", "Version") .. ".")
+		print("It looks like you're trying to use Clippy Assist. " .. 
+			"Would you like some help with that?")
+		print("  -help: Shows this text.")
+		print("  -reset: Resets Clippy's position.")
+		print("  -list: Lists all available animations.")
+		print("  everything else: Attempts to play that animation.")
+	elseif msg == "-reset" or msg == "-r" then
+	elseif msg == "-list" or msg == "-l" then
+	else
+		if data.msg == nil then
+			print("Couldn't find that animation.")
+			print("Use \"/clippy -list\" to list available animations.")
+			print("Use \"/clippy -help\" to view all commands.")
+		else
+			QueueAnimation(msg)
+		end
+	end
 end
